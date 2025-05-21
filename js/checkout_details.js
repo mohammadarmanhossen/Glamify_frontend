3031ab94bd022d98e2be1bb194c6124a27ea3e2c
arman <mdarmanhossen404@gmail.com>
2025-05-21 08:03:24 +0600

    Glamify_backend

--- js/checkout_details.js
+++ js/checkout_details.js
@@ -1,9 +1,9 @@
- // আজকের তারিখ নিয়ে formatted string বানানো
+
   const today = new Date();
   
-  // format: 17 May, 2025
+
   const options = { year: 'numeric', month: 'long', day: 'numeric' };
   const formattedDate = today.toLocaleDateString('en-US', options);
   
-  // স্প্যান এ সেট করা
+
   document.getElementById('invoice-date').textContent = formattedDate;
