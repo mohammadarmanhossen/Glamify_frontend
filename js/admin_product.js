471d6373649bd6da190c552df0db70682c175c02
arman <mdarmanhossen404@gmail.com>
2025-05-23 23:42:27 +0600

    Glamify_frontend

--- js/admin_product.js
+++ js/admin_product.js
@@ -1,22 +1,21 @@
-
-
 const adminProduct = () => {
-    fetch('https://glamify-backend-ten.vercel.app/product/', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        const productBody = document.getElementById('product-body');
-        productBody.innerHTML = '';
+  fetch("https://glamify-backend-ten.vercel.app/product/", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      data.sort((a, b) => b.id - a.id);
+      const productBody = document.getElementById("product-body");
+      productBody.innerHTML = "";
 
-        data.forEach(product => {
-            const row = document.createElement('tr');
-            row.classList.add('border-b');
+      data.forEach((product) => {
+        const row = document.createElement("tr");
+        row.classList.add("border-b");
 
-            row.innerHTML = `
+        row.innerHTML = `
                 <td class="p-2">${product.id}</td>
                 <td class="p-2">${product.name}</td>
                 <td class="p-2">${product.description}</td>
@@ -31,87 +30,82 @@ const adminProduct = () => {
                
             `;
 
-            productBody.appendChild(row);
-        });
+        productBody.appendChild(row);
+      });
 
-        const deleteButtons = document.querySelectorAll('.delete-button');
-        deleteButtons.forEach(button => {
-            button.addEventListener('click', () => {
-                const productId = button.getAttribute('data-id');
-                deleteProduct(productId);
-            });
+      const deleteButtons = document.querySelectorAll(".delete-button");
+      deleteButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+          const productId = button.getAttribute("data-id");
+          deleteProduct(productId);
         });
+      });
     })
     .catch((error) => {
-        console.error('Error:', error);
+      console.error("Error:", error);
     });
 };
 
 function deleteProduct(productId) {
-    fetch(`https://glamify-backend-code.onrender.com/product/${productId}/`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-        }
+  fetch(`https://glamify-backend-code.onrender.com/product/${productId}/`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        adminProduct();
+      } else {
+        alert("Failed to delete product.");
+      }
     })
-    .then(response => {
-        if (response.ok) {
-            adminProduct(); 
-        } else {
-            alert('Failed to delete product.');
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('An error occurred while deleting the product.');
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("An error occurred while deleting the product.");
     });
 }
 
 adminProduct();
 
-
-
-
-
-
-document.getElementById('product-form').addEventListener('submit', function (e) {
+document
+  .getElementById("product-form")
+  .addEventListener("submit", function (e) {
     e.preventDefault();
 
     const productData = {
-        name: document.getElementById('name').value,
-        description: document.getElementById('description').value,
-        price: parseFloat(document.getElementById('price').value),
-        stock: parseInt(document.getElementById('stock').value),
-        image_url: document.getElementById('image_url').value,
-        brand: document.getElementById('brand').value
+      name: document.getElementById("name").value,
+      description: document.getElementById("description").value,
+      price: parseFloat(document.getElementById("price").value),
+      stock: parseInt(document.getElementById("stock").value),
+      image_url: document.getElementById("image_url").value,
+      brand: document.getElementById("brand").value,
     };
     console.log(productData);
 
-    fetch('https://glamify-backend-code.onrender.com/product/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(productData)
+    fetch("https://glamify-backend-code.onrender.com/product/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
     })
-    .then(response => {
+      .then((response) => {
         if (response.ok) {
-            Swal.fire({
-                title: "Success!",
-                text: "Added product successfully ",
-                icon: "success",
-                confirmButtonText: "OK",
-              })
-            adminProduct(); 
-            e.target.reset();
+          Swal.fire({
+            title: "Success!",
+            text: "Added product successfully ",
+            icon: "success",
+            confirmButtonText: "OK",
+          });
+          adminProduct();
+          e.target.reset();
         } else {
-            alert('Failed to add product');
+          alert("Failed to add product");
         }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('Error adding product');
-    });
-});
-
-
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("Error adding product");
+      });
+  });
