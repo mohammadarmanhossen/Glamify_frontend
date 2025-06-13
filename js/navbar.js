23ae3caa49f299b1c23e3e21ec278a77067a0f0e
arman <mdarmanhossen404@gmail.com>
2025-06-13 12:36:09 +0600

    Glamify_frontend

--- js/navbar.js
+++ js/navbar.js
@@ -17,7 +17,8 @@ const navbar = () => {
          <div class="dropdownMenu hidden absolute right-0 mt-2 w-44 bg-white border border-gray-200 rounded text-center shadow-lg z-50 divide-y divide-gray-100">
 
             <a href="/profile.html" class="flex items-center justify-center gap-2 w-full px-6 py-2 text-black font-semibold hover:bg-gray-100 transition duration-200">
-             👤 Profile
+               <img src="image/account.svg" alt="Account Icon" class="w-5 h-5">
+                Profile
             </a>
 
              <a href="/order.html" class="flex items-center justify-center gap-2 w-full px-6 py-2 text-black font-semibold hover:bg-gray-100 transition duration-200">
@@ -65,10 +66,11 @@ const mobilenavbar = () => {
                   <img src="image/account.svg" alt="Account Icon" class="w-5 h-5">
                   <span>Account</span>
               </button>
-          <div class="dropdownMenu hidden absolute right-0 mt-2 w-40  border border-gray-200  rounded z-50">
+          <div class="dropdownMenu hidden absolute right-0 mt-2 w-40  border border-gray-200 bg-gray-100 rounded z-50">
                   
             <a href="/profile.html" class="flex items-center justify-center gap-2 w-full px-6 py-2 text-black font-semibold hover:bg-gray-100 transition duration-200">
-             👤 Profile
+              <img src="image/account.svg" alt="Account Icon" class="w-5 h-5">
+               Profile
             </a>
 
            <a href="/order.html" class="flex items-center justify-center gap-2 w-full px-6 py-2 text-black font-semibold hover:bg-gray-100 transition duration-200">
@@ -104,7 +106,7 @@ const mobilenavbar = () => {
 navbar();
 mobilenavbar();
 
-// Handle dropdown toggling
+
 document.addEventListener("click", function (e) {
   if (e.target.closest(".dropdownButton")) {
     const dropdown = e.target
@@ -114,9 +116,10 @@ document.addEventListener("click", function (e) {
   }
 });
 
-// Mobile menu toggle
+
 const menuBtn = document.getElementById("menu-btn");
 const mobileMenu = document.getElementById("mobile-menu");
 menuBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 });
+  
